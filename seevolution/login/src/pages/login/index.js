2af6891b4159f101d74bf0ab04d885dddc953a10
ISADORA PIGATO VELHO <IPV11653@intralesc.sc.gov.br>
2023-11-28 14:28:12 -0300

    ntro

--- seevolution/login/src/pages/login/index.js
+++ seevolution/login/src/pages/login/index.js
@@ -1,170 +1,82 @@
 import React from "react"
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
+import { View, Text, StyleSheet, TextInput, Image, TouchableOpacity } from "react-native"
 import { useNavigation } from '@react-navigation/native'
-import { TextInput, } from 'react-native-paper'
-import see from '../../assets/see.png'
-
 import * as Animatable from 'react-native-animatable'
 import Cadastro from "../cadastro"
-import Evolution from "../evolution"
+import Undraw from "../../assets/undraw.png"
 
-
-export default function Login() {
+export default function Intro() {
   const navigation = useNavigation()
-  const [text, setText] = React.useState("");
   return (
     <View style={styles.container}>
       <Animatable.View
-        animation="fadeInLeft"
-        delay={500}
-        style={styles.containerHeader}
-      >
-
-      </Animatable.View>
-
-      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+       
+      > 
       <Image
-        style={styles.SeeLogo}
-        source={require('../../assets/see.png')}
+        style={styles.undraw}
+        source={require('../../assets/undraw.png')}
       />
-        <View style={styles.header}>
-          <Text style={styles.message}>Olá, novamente</Text>
-          <Text style={styles.message2}>Faça login</Text>
-        </View>
-
-        <TextInput
-          mode='outlined'
-          cancelable='true'
-          style={styles.input}
-          keyboardType='default'
-          label="E-mail"
-          outlineColor='#AE001B'
-          selectionColor='#AE001B'
-          onChangeText={text => setText(text)}
-          activeOutlineColor='#AE001B'
-        />
-
-        <TextInput
-          mode='outlined'
-          cancelable='true'
-          style={styles.input}
-          keyboardType='default'
-          label="Senha"
-          outlineColor='#AE001B'
-          selectionColor='#AE001B'
-          onChangeText={text => setText(text)}
-          activeOutlineColor='#AE001B'
-        />
-        
-
-        <TouchableOpacity style={styles.button}
-          onPress={() => navigation.navigate(Evolution)}>
-          <Text style={styles.buttonText}>Continuar</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.buttonRegister}
-          onPress={() => navigation.navigate(Cadastro)}>
-          <View style={styles.textos}>
-            <Text style={styles.registerText}>Não tem uma conta?</Text>
-            <Text style={styles.registerText2}> Crie uma</Text>
-
-          </View>
-
-        </TouchableOpacity>
+        <Text style={styles.message}> Evolução</Text>
+        <Text style={styles.message2}> O intuito do projeto é trazer facilidade às pessoas para consumirem conteúdos e estratégias para sua evolução. Além de {`\n`} motivar e mostrar o avanço/crescimento{`\n`} dos seus esforços e dedicações.</Text>
       </Animatable.View>
+
+      <TouchableOpacity style={styles.button}
+          onPress={() => navigation.navigate(Cadastro)}>
+          <Text style={styles.registerText}>Começar</Text>
+        </TouchableOpacity>
+
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    alignItems: 'center',
     flex: 1,
     backgroundColor: '#0B1A1F'
   },
-  SeeLogo:{
-    marginTop: '10%',
-    width: 65,
-    height: 65,
-    marginRight:'83%'
-  },
   containerHeader: {
-    marginTop: '13%',
-    marginBottom: '8%',
-    paddingStart: '5%'
+   
   },
-  header: {
-    width: '100%',
-    height: 'auto',
-    display: 'flex',
-    alignItems:'flex-start'
-    
+  undraw:{
+    marginLeft: 60,
+    marginRight: 60,
+    marginTop: '30%',
+    width: 260,
+    height: 260,
+    marginBottom: 20,
   },
   message: {
-    marginTop: 8,
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#AE001B'
+    textAlign: 'center',
+    fontSize: 25,
+    marginLeft: 20,
+    marginRight: 40,
+    color: '#fff',
+    fontWeight: 'bold'
   },
   message2: {
-    fontSize: 15,
-    color: '#AE001B'
+    
+    textAlign: 'center',
+    fontSize: 17,
+    marginLeft: 20,
+    marginRight: 40,
+    color: '#fff'
+    /*fontWeight: 'bold'*/
   },
-  containerForm: {
-    backgroundColor: '#fff',
-    flex: 1,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    paddingStart: '5%',
-    paddingEnd: '5%',
-    display: 'flex',
+  registerText:{
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
     alignItems: 'center'
   },
-  title: {
-    fontSize: 20,
-    marginTop: 28,
-    color: '#AE001B'
-  },
-  input: {
-    width: '80%',
-    height: 50,
-    marginBottom: 12,
-    fontSize: 16,
-    placeholderTextColor: '#000',
-    marginHorizontal: 50,
-    marginVertical: 20,
-    outlineColor: '#AE001B'
-  },
+
   button: {
     backgroundColor: '#AE001B',
     width: '80%',
     borderRadius: 4,
     paddingVertical: 8,
-    marginTop: 14,
+    marginTop:115,
     justifyContent: 'center',
     alignItems: 'center'
-
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  buttonRegister: {
-    marginTop: 14,
-    alignItems: 'center'
-  },
-  registerText: {
-    color: '#AE001B',
-    fontWeight: 'bold'
-  },
-  registerText2: {
-    color: '#AE001B',
-    fontWeight: 'bold'
-  },
-  textos: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-
+  }
 })
